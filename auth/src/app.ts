2c1bcef7d60a46039b9c09f10bb98e6e3238cef9
hatsukoi <hatsukoi.jiang@gmail.com>
2024-01-02 16:21:12 -0800

    Add test

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { errorHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    secure: process.env.NODE_ENV !== 'test',
+    signed: false,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
