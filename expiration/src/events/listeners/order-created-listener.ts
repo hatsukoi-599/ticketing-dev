9bc0e8b790a069407ed93c6f7c4e4f4fe8ed4a26
hatsukoi <hatsukoi.jiang@gmail.com>
2024-02-21 18:13:12 -0800

    init expiration service

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,24 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from '@xjtickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(
+    data: {
+      id: string;
+      version: number;
+      status: OrderStatus;
+      userId: string;
+      expiresAt: string;
+      ticket: { id: string; price: number };
+    },
+    msg: Message
+  ) {}
+}
